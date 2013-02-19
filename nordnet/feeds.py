3bc8fd7723716d0667ccdd92078bf26f0db66e10
Emil Stenberg <esandin@gmail.com>
2013-02-19 22:27:18 +0100

    Checking that input is cleared after each handle

--- nordnet/feeds.py
+++ nordnet/feeds.py
@@ -1,7 +1,17 @@
 class FeedInputHandler():
-	def handle_input(self, *args, **kwargs):
-		pass
-		#self.postData()
+	def __init__(self):
+		self.buffered_input = ''
 
-	def postData(self):
+	def handle_input(self, input):
+		self.buffered_input = self.buffered_input + input
+		split_input = self.buffered_input.split('\n')
+		complete_chunks = split_input[0:-1]
+		
+		for chunk in complete_chunks:
+			print "handle chunk %s" % chunk
+			self.handle_data_chunk(chunk)
+
+		# TODO remove the handled chunks from buffered input
+
+	def handle_data_chunk(self, chunk):
 		pass
--- nordnet/tests/test_reading_feed.py
+++ nordnet/tests/test_reading_feed.py
@@ -1,21 +1,45 @@
 from nose.tools import *
 from nordnet.feeds import *
-from mock import Mock, MagicMock
+#from mock import Mock, MagicMock
+from exam.mock import Mock
 
+def _test_handle_data_chunk():
+	ok_(False)
 
 def test_read_part_of_message():
 	msg1 = "{"
-	msg2 = "   "
-	msg3 = "}"
+	msg2 = "1"
+	msg3 = "}\n{"
+	msg4 = "2}\n"
+	msg5 = "{3}\n"
 
 	ih = FeedInputHandler()
-	ih.postData = Mock()
+	ih.handle_data_chunk = Mock()
 	
 	ih.handle_input(msg1)
-	ok_(not ih.postData.called)
+	ok_(not ih.handle_data_chunk.called)
 
 	ih.handle_input(msg2)
-	ok_(not ih.postData.called)
+	ok_(not ih.handle_data_chunk.called)
 
-	ih.handle_input(msg1)
-	ih.postData.assert_called_with( msg1 + msg2 + msg3)
+	ih.handle_input(msg3)
+	ih.handle_data_chunk.assert_called_with( '{1}')
+
+	ih.handle_data_chunk.reset_mock()
+	ih.handle_input(msg4)
+	ih.handle_data_chunk.assert_called_with( '{2}')
+	#ih.handle_data_chunk.assert_not_called_with( '{1}')
+
+	############
+	# Can not get assert_not_called_with to work, aserting the
+	# buffered input instead
+	ok_(ih.buffered_input == '', "input should be empty, but was '%s'" % ih.buffered_input)
+	print "+"*35
+	print "buffered input: '%s'" % ih.buffered_input
+
+	ih.handle_input(msg5)
+	#ih.handle_data_chunk.assert_not_called_with( '{1}')
+	#ih.handle_data_chunk.assert_not_called_with( '{2}')
+	ih.handle_data_chunk.assert_called_with( '{3}')
+	
+	
